09d3f70e02722885a0a00162f6cf66a8c9a174ba
Ximena Castaño Grisales <xime.grisales@gmail.com>
2025-05-01 15:58:50 +0200

    cambios definitivos

--- Base de datos musicstream.sql
+++ Base de datos musicstream.sql
@@ -3,43 +3,43 @@ CREATE DATABASE musicstream;
 USE musicstream;
 
 CREATE TABLE IF NOT EXISTS artists (
-    artist_id VARCHAR(500) PRIMARY KEY,
-    artist_name VARCHAR(500) NOT NULL,
-    listeners INT,
-    reproductions INT,
+    artist_id VARCHAR(250) PRIMARY KEY,
+    artist_name VARCHAR(250),
+    listeners BIGINT,
+    reproductions BIGINT,
     biography TEXT
 );
 
 CREATE TABLE IF NOT EXISTS albums (
-    album_id VARCHAR(500) PRIMARY KEY,
-    album_name VARCHAR(100) NOT NULL,
+    album_id VARCHAR(250) PRIMARY KEY,
+    album_name VARCHAR(250),
     release_date DATE,
     total_tracks INT,
-    type_albums VARCHAR(20),
-    popularity INT
+    type_albums VARCHAR(100),
+    popularity FLOAT
 );
 
 CREATE TABLE IF NOT EXISTS tracks (
-    track_id VARCHAR(500) PRIMARY KEY,
-    track_name VARCHAR(500) NOT NULL,
-    artist_id VARCHAR(500),
-    album_id VARCHAR(500),
-    duration_ms INT,
-    popularity INT,
+    track_id VARCHAR(250) PRIMARY KEY,
+    track_name VARCHAR(250),
+    artist_id VARCHAR(250),
+    album_id VARCHAR(250),
+    duration_ms FLOAT,
+    popularity FLOAT,
     release_date DATE,
-    type_tracks VARCHAR(20),
+    type_tracks VARCHAR(200),
     FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
     FOREIGN KEY (album_id) REFERENCES albums(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS genres (
-    genre_id VARCHAR(500) PRIMARY KEY,
-    genre_name VARCHAR(500) NOT NULL
+    genre_id INT PRIMARY KEY,
+    genre_name VARCHAR(250)
 );
 
 CREATE TABLE IF NOT EXISTS artist_genres (
     artist_id VARCHAR(300),
-    genre_id INT AUTO_INCREMENT,
+    genre_id INT,
     PRIMARY KEY (artist_id, genre_id),
     FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
     FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
@@ -47,15 +47,15 @@ CREATE TABLE IF NOT EXISTS artist_genres (
 
 CREATE TABLE IF NOT EXISTS genre_albums (
 	album_id VARCHAR(300),
-    genre_id VARCHAR(300),
+    genre_id INT,
     PRIMARY KEY (album_id, genre_id),
 	FOREIGN KEY (album_id) REFERENCES albums(album_id),
     FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
 );
 
 CREATE TABLE IF NOT EXISTS market (
-	market_id VARCHAR(500) PRIMARY KEY,
-    market_name VARCHAR (500)
+	market_id VARCHAR(250) PRIMARY KEY,
+    market_name VARCHAR (250)
 );
 
 CREATE TABLE IF NOT EXISTS market_track (
@@ -75,11 +75,17 @@ CREATE TABLE IF NOT EXISTS market_album (
 );
 
 CREATE TABLE IF NOT EXISTS similar_artists (
-	artist_id VARCHAR(500) PRIMARY KEY, 
+	artist_id VARCHAR(250) PRIMARY KEY, 
     similar_artirst_name TEXT,
     FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 );
 
-SELECT * FROM artists;
+ALTER TABLE albums MODIFY COLUMN release_date DATE;
 
-SELECT * FROM artist_genres
+ALTER TABLE tracks MODIFY COLUMN release_date DATE;
+
+SELECT COUNT(*) FROM artists;
+
+SELECT * FROM tracks LIMIT 5;
+
+SELECT * FROM genre_albums LIMIT 5;
